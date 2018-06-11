2d55ac3d9d282c0285c9b5917d33d642074991fd
bum752 <bum752@gmail.com>
2018-06-11 12:31:16 +0900

    Initial commit

---
+++ src/main/java/com/jbshin/mahout/MahoutApplication.java
@@ -0,0 +1,13 @@
+package com.jbshin.mahout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahoutApplication.class, args);
+	}
+
+}
